cfa94e788331fe01a508862160281e19f5e23c54
Hristo Karchev <x_karchev@mail.bg>
2022-03-10 22:03:37 +0200

    cleaning

---
+++ src/components/LearningInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>aasdd</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LearningInfo",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
